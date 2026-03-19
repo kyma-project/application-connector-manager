232e1a913921edc9319e07fe17f0cdf8814c8a47
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2026-03-19 19:39:49 +0100

    add new test

--- controllers/applicationconnector_controller_test.go
+++ controllers/applicationconnector_controller_test.go
@@ -110,6 +110,8 @@ func testInstanceUpdate(t time.Duration, ac v1alpha1.ApplicationConnector, updat
 	By(fmt.Sprintf("create application-connector instance: %s/%s", ac.Namespace, ac.Name))
 	Expect(k8sClient.Create(ctx, &ac)).To(Succeed())
 
+	ac.Status.State = ""
+
 	testReconcile(ac, ctx, t)
 	updateFunc(&ac)
 	testReconcile(ac, ctx, t)
