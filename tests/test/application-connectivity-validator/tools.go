87f1f328d58d34d06ae056bc02a3029339275572
Rafal Foks <r.fok74@gmail.com>
2026-05-26 12:25:07 +0200

    Rollback to orginal structure, update to the newest tests

--- tests/test/application-connectivity-validator/tools.go
+++ tests/test/application-connectivity-validator/tools.go
@@ -2,5 +2,4 @@ package application_connectivity_validator
 
 func validatorURL(app, path string) string {
 	return "http://central-application-connectivity-validator.kyma-system:8080/" + app + "/" + path
-	// trigger build
 }
